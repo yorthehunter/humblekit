f8709efd791c861183ca74fa875468e238694345
yorthehunter <boru.design@gmail.com>
2014-03-06 18:55:05 -0500

    init typemouth

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Typemouth::Application.config.secret_key_base = 'd86881466214605bd2a0ff90546b3afb5b2fdc1bdea2cf7725e6ae9bae441ebc6193fca30c4fc4d219325c8bed3a07e7b8aa67d8878d780d849663380a49f693'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Typemouth::Application.config.session_store :cookie_store, key: '_typemouth_session'

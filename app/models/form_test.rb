1b6abc757454b7137ea8eefe4f69ccb9d1b23bba
yorthehunter <boru.design@gmail.com>
2014-04-17 17:14:24 -0400

    psh

--- app/models/form_test.rb
+++
@@ -1,9 +0,0 @@
-class FormTest < ApplicationController
-  attribute :name
-  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-  attribute :message
-  attribute :check1
-  attribute :check2
-  attribute :radio2
-  attribute :radio2
-end
